88db998f605f5e3dba3cae54c15420e7c9e3f6e0
Gustavo <gustavogomes1108@gmail.com>
2025-05-21 12:49:36 -0300

    .

--- Estrutura de dados _Pilha.c
+++ Estrutura de dados _Pilha.c
@@ -14,35 +14,89 @@ void imprimir();
 int entrada_dados();
 
 int main(){
-	int opcao;
+	int dado, opcao;
 	
-	system("cls");
+	do {
+		system("cls");
 	
-	printf("Sistema com estrutura em PILHA - Por Mr.JeeJ\n");
-	printf("1 - Empilhar\n2 - Desempilhar\n3 - Imprimir\n0 - Sair\n");
-	
-	printf("Escolha uma opcao: \n");
-	scanf("%d", &opcao);
-	
-	switch(opcao){
-		case 0:
-			system("pause");
-		break;
+		printf("Sistema com estrutura em PILHA - Por Mr.JeeJ\n");
+		printf("1 - Empilhar\n2 - Desempilhar\n3 - Imprimir\n0 - Sair\n");
 		
-		case 1:
+		printf("Escolha uma opcao: ");
+		scanf("%d", &opcao);
+		
+		switch(opcao){	
+			case 1:
+				dado = entrada_dados();
+				push(dado);
+			break;
 			
-		break;
-		
-		
-		case 2:
+			case 2:
+				pop();	
+			break;
 			
-		break;
+			case 3:
+				imprimir();
+			break;
+		}	
 		
-		case 3:
-			
-		break;
-	}
-		
+		system("pause");
+	} while (opcao != 0);
+	
 	return 0;
 }
 
+int entrada_dados(){
+	system("cls");
+	
+	int n;
+	
+	printf("Qual o numero que sera guardado?: ");
+	scanf("%d", &n);
+	
+	return n;	
+}
+
+void push(int item){
+	No *novo = malloc(sizeof(No));
+	(*novo).dado = item;
+	(*novo).proximo = top;
+	
+	top = novo;
+	
+	printf("Valor empilhado!\n");
+}
+
+void pop(){
+	system("cls");
+	
+	if (top != NULL){
+		No *temp = top;
+		top = (*top).proximo;
+		
+		free(temp);
+		
+		printf("Valor desempilhado\n");	
+	} else {
+		printf("A pilha ja esta vazia!\n");
+	}
+}
+
+void imprimir(){
+	system("cls");	
+
+	printf("Pilha completa\n\n");
+	
+	if (top != NULL){
+		No *temp = top;	
+		
+		while (temp != NULL){
+			printf("%d\n", (*temp).dado);
+			
+			temp = (*temp).proximo;
+		}
+		
+	} else {
+		printf("A pilha esta vazia!\n");
+	}
+}
